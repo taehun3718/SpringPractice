9185f98887b5c629bb0d115e5db697b0c1e917a8
KTDS_Christof_Kim <taehun3718@gmail.com>
2015-04-03 10:46:12 +0900

    ModelAndView

--- TestMVC/src/main/java/com/ktds/christof_kim/web/TestController.java
+++ TestMVC/src/main/java/com/ktds/christof_kim/web/TestController.java
@@ -2,6 +2,8 @@ package com.ktds.christof_kim.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -14,5 +16,35 @@ public class TestController {
 		// /WEB-INF/view/test/test.jsp
 		return "test/test";
 	}
+	
+	@RequestMapping("/test2")
+	public ModelAndView test2() {
+		
+		ModelAndView view = new ModelAndView();
+		
+		view.setViewName("test/test2");
+		view.addObject("helloMessage", "Hello!");	//Key, Value
+		view.addObject("byeMessage", "bye bye");
+		
+		//뷰를 리턴하면 jsp를 리턴하게 되어 있음
+		return view;
+		
+	}
+	
+	@RequestMapping(value="/test4", method=RequestMethod.GET)
+	public ModelAndView test4() {
+		
+		ModelAndView view = new ModelAndView();
+		view.setViewName("test/test2");
+		return view;
+	}
+	
+	@RequestMapping(value="/test5", method=RequestMethod.POST)
+	public ModelAndView test5() {
+		
+		ModelAndView view = new ModelAndView();
+		return view;
+	}
+	
 	
 }
