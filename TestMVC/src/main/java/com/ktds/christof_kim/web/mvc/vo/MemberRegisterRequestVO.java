64ee4489e9bbeead6dad3426ffaca6e269c4e739
KTDS_Christof_Kim <taehun3718@gmail.com>
2015-04-03 14:08:08 +0900

    2015-04-03_MVC param_validation check

---
+++ TestMVC/src/main/java/com/ktds/christof_kim/web/mvc/vo/MemberRegisterRequestVO.java
@@ -0,0 +1,56 @@
+package com.ktds.christof_kim.web.mvc.vo;
+
+import java.util.List;
+
+public class MemberRegisterRequestVO {
+
+	
+	private boolean isCheck;
+	
+	private List<String >names;
+	
+	private String email;
+	private String name;
+	private String password;
+	private String confirmPassword;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	public boolean isCheck() {
+		return isCheck;
+	}
+	public void setIsCheck(boolean isCheck) {
+		this.isCheck = isCheck;
+	}
+	public List<String> getNames() {
+		return names;
+	}
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+
+
+}
--- TestMVC/src/main/webapp/WEB-INF/view/test/test2.jsp
+++ TestMVC/src/main/webapp/WEB-INF/view/test/test2.jsp
@@ -13,5 +13,32 @@ ${byeMessage }
 <form method="post" action="/TestMVC/test5">
 	<input type="submit" value="전송"/>
 </form>
+
+
+<a href="/TestMVC/test7?id=abc">전송test7!</a>
+<form method="post" action="/TestMVC/test7">
+	<input type="text" name="id"/>
+	<input type="submit" value="전송!"/>
+</form>
+
+
+<a href="/TestMVC/test7?id=abc">전송test9!</a>
+<form method="post" action="/TestMVC/test9">
+
+	체크드<input type="checkbox" name="isCheck" value="true"/><br>
+
+	Names:<input type="text" name="names"/><br/>
+	Names:<input type="text" name="names"/><br/>
+	Names:<input type="text" name="names"/><br/>
+	Names:<input type="text" name="names"/><br/>
+				
+
+	Email:<input type="text" name="email"/><br/>
+	name:<input type="text" name="name"/><br/>
+	Password<input type="text" name="password"/><br/>
+	
+	<input type="submit" value="전송!"/>
+</form>
+
 </body>
 </html>
